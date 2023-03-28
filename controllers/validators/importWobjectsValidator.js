14f0a0d416e7cc3abc6dd8bd495c991c84b073b9
fesmofet <borees2@gmail.com>
2023-03-28 10:22:22 +0300

    check bot rc

--- controllers/validators/importWobjectsValidator.js
+++ controllers/validators/importWobjectsValidator.js
@@ -8,6 +8,7 @@ exports.importDatafinityObjectsSchema = Joi.object().keys({
   authority: Joi.string().valid(...Object.values(AUTHORITY_FIELD_OPTIONS)),
   locale: Joi.string().valid(...Object.keys(FIELD_LANGUAGES_TO_NLP)).default('en-US'),
   translate: Joi.boolean().default(false),
+  useGPT: Joi.boolean().default(false),
 });
 
 exports.importStatisticsSchema = Joi.object().keys({
