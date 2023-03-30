4c34b002e16369df99a655564d2956b40c810d93
fesmofet <borees2@gmail.com>
2023-03-30 10:10:32 +0300

    graceful shutdown

--- database/index.js
+++ database/index.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const config = require('../config');
+const importDb = require('../importObjectsDB/importObjects_Connection');
 
 const URI = `mongodb://${config.db.host}:${config.db.port}/${config.db.database}`;
 
@@ -7,13 +8,19 @@ mongoose.connect(URI, { useNewUrlParser: true, useFindAndModify: false, useUnifi
   .then(() => console.log('MongoDB connection successful!'))
   .catch((error) => console.error(error));
 
-
 mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection error:'));
+mongoose.connection.on('close', () => console.log(`closed ${config.db.database}`));
 
 mongoose.Promise = global.Promise;
 
+const closeMongoConnections = async () => {
+  await mongoose.connection.close(false);
+  await importDb.close(false);
+};
+
 module.exports = {
   Mongoose: mongoose,
+  closeMongoConnections,
   models: {
     WObject: require('./schemas/wObjectSchema'),
     ObjectType: require('./schemas/ObjectTypeSchema'),
--- importObjectsDB/importObjects_Connection.js
+++ importObjectsDB/importObjects_Connection.js
@@ -2,8 +2,10 @@ const mongoose = require('mongoose');
 const config = require('../config');
 
 const URI = `mongodb://${config.importDB.host}:${config.importDB.port}/${config.importDB.database}`;
-
-module.exports = mongoose.createConnection(URI, {
+const importDb = mongoose.createConnection(URI, {
   useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: false,
 },
 () => console.log('ImportObjects DB connection successful!'));
+importDb.on('close', () => console.log(`closed ${config.importDB.database}`));
+
+module.exports = importDb;
