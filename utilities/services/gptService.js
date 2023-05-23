642cd34516b1f464f258cdc9d9342ba1b8eb6a50
fesmofet <borees2@gmail.com>
2023-05-23 11:11:07 +0300

    improve gpt prompts

--- utilities/services/gptService.js
+++ utilities/services/gptService.js
@@ -15,8 +15,12 @@ const openai = new OpenAIApi(configurationImport);
 const openaiBot = new OpenAIApi(configurationBot);
 
 const GPT_CRAFTED = ' Description by ChatGPT.';
+const BASIC_PROMPT = 'provide me with some basic information about';
+const QUESTION_PROMPT = 'do you have any information about';
 
 const checkForPositiveAnswer = (answer = '') => answer.toLowerCase().includes('yes');
+
+const checkAiResponse = (answer = '') => answer.toLowerCase().includes('as an ai language model');
 
 const gptCreateCompletion = async ({ content = '' }) => {
   try {
@@ -64,48 +68,52 @@ const makeDescription = async (description = '') => {
 
 const makeProductDescription = async (product = '') => {
   const { result: firstResponse, error: answerError } = await gptCreateCompletion({
-    content: `do you have any information about product ${product}, answer only yes or no`,
+    content: `${QUESTION_PROMPT} product ${product}, answer only yes or no`,
   });
   if (answerError) return '';
   const positiveAnswer = checkForPositiveAnswer(firstResponse);
   if (!positiveAnswer) return '';
 
   const { result, error } = await gptCreateCompletion({
-    content: `tell me more about ${product}`,
+    content: `${BASIC_PROMPT} ${product}`,
   });
+
   if (!result || error) return '';
+  if (checkAiResponse(result)) return '';
   return `${result}${GPT_CRAFTED}`;
 };
 
 const makeAuthorDescription = async ({ author = '', book = '' }) => {
   const { result: firstResponse, error: answerError } = await gptCreateCompletion({
-    content: `do you have any information about ${author}, the author of ${book}, answer only yes or no`,
+    content: `${QUESTION_PROMPT} ${author}, the author of ${book}, answer only yes or no`,
   });
   if (answerError) return '';
   const positiveAnswer = checkForPositiveAnswer(firstResponse);
   if (!positiveAnswer) return '';
 
   const { result, error } = await gptCreateCompletion({
-    content: `tell me more about ${author}, the author of ${book}`,
+    content: `${BASIC_PROMPT} ${author}, the author of ${book}`,
   });
 
   if (!result || error) return '';
+  if (checkAiResponse(result)) return '';
   return `${result}${GPT_CRAFTED}`;
 };
 
 const makeBookDescription = async ({ author = '', book = '' }) => {
   const { result: firstResponse, error: answerError } = await gptCreateCompletion({
-    content: `do you have any information about book ${book}, by ${author}, answer only yes or no`,
+    content: `${QUESTION_PROMPT} book ${book}, by ${author}, answer only yes or no`,
   });
   if (answerError) return '';
   const positiveAnswer = checkForPositiveAnswer(firstResponse);
   if (!positiveAnswer) return '';
 
   const { result, error } = await gptCreateCompletion({
-    content: `tell me more about book ${book}, by ${author}`,
+    content: `${BASIC_PROMPT} ${book}, by ${author}`,
   });
 
   if (!result || error) return '';
+  if (checkAiResponse(result)) return '';
   return `${result}${GPT_CRAFTED}`;
 };
 
