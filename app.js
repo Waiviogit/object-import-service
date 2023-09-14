a42c9e2c17598396c8d6910df94d525f27b935cb
fesmofet <borees2@gmail.com>
2023-09-14 13:35:24 +0300

    test parser

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const logger = require('morgan');
 const dotenv = require('dotenv');
 const cors = require('cors');
 const swaggerUi = require('swagger-ui-express');
+const bodyParser = require('body-parser');
 const { routes } = require('./routes');
 const startup = require('./utilities/helpers/startupHelper');
 require('./utilities/redis/subscriber/subscriber');
@@ -13,18 +14,13 @@ dotenv.config();
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.use(cors());
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+
 app.use('/', routes);
 app.use('/import-objects-service/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
 
 app.use((err, req, res, next) => {
   if (!err.status) {
