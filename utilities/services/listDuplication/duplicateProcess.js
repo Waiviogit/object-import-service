3aec1c7cf60125721798661aa0de42bcac790f0f
fesmofet <borees2@gmail.com>
2023-11-01 11:33:59 +0200

    6065

--- utilities/services/listDuplication/duplicateProcess.js
+++ utilities/services/listDuplication/duplicateProcess.js
@@ -120,7 +120,7 @@ const checkFieldsToVote = async ({ importId }) => {
 
 const promptsByFieldName = {
   name: 'rewrite name of a product',
-  title: 'Using your best SEO and copywriting skills, rephrase the title in 1 sentence',
+  title: 'rephrase following title in 1 sentence',
   description: 'rewrite description  seo friendly, act as a professional copywriter and seo expert, 3 paragraph max',
 };
 
