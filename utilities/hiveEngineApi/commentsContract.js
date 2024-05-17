f9f5f68a7f16ad9bc33d238c48565e0796c99f63
fesmofet <borees2@gmail.com>
2024-05-17 22:50:45 +0300

    fix new account

--- utilities/hiveEngineApi/commentsContract.js
+++ utilities/hiveEngineApi/commentsContract.js
@@ -1,11 +1,22 @@
+const _ = require('lodash');
 const { engineProxy } = require('./hiveEngineApi');
 
-exports.getVotingPower = async ({ rewardPoolId, account, method }) => engineProxy({
-  endpoint: '/contracts',
-  params: {
-    contract: 'comments',
-    table: 'votingPower',
-    query: { rewardPoolId, account },
-  },
-  method,
-});
+exports.getVotingPower = async ({ rewardPoolId, account, method }) => {
+  const result = await engineProxy({
+    endpoint: '/contracts',
+    params: {
+      contract: 'comments',
+      table: 'votingPower',
+      query: { rewardPoolId, account },
+    },
+    method,
+  });
+
+  const onEmptyResp = {
+    votingPower: 10000,
+    downvotingPower: 10000,
+    lastVoteTimestamp: Date.now(),
+  };
+  if (_.isEmpty(result)) return [onEmptyResp];
+  return result;
+};
