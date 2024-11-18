94c3b13d27c3aff2a34c689e9f8a74eff1e9b1ef
fesmofet <borees2@gmail.com>
2024-11-18 16:31:23 +0200

    post host

--- controllers/validators/postImportValidator.js
+++ controllers/validators/postImportValidator.js
@@ -2,7 +2,7 @@ const Joi = require('joi');
 
 exports.postImportSchema = Joi.object().keys({
   user: Joi.string().required(),
-  host: Joi.string(),
+  host: Joi.string().hostname(),
   dailyLimit: Joi.number().integer().min(0).default(0),
   posts: Joi.array().items(Joi.object().keys({
     title: Joi.string().required(),
@@ -12,6 +12,6 @@ exports.postImportSchema = Joi.object().keys({
 });
 
 exports.postHostSchema = Joi.object().keys({
-  host: Joi.string(),
+  host: Joi.string().hostname().required(),
   user: Joi.string().required(),
 });
