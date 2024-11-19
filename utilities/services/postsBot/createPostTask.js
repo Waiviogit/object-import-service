3e443cb0c4ebf18693d98c810e49e6594036b895
fesmofet <borees2@gmail.com>
2024-11-19 12:00:47 +0200

    fix status post import

--- utilities/services/postsBot/createPostTask.js
+++ utilities/services/postsBot/createPostTask.js
@@ -3,11 +3,22 @@ const {
 } = require('../../../models');
 const { createUUID } = require('../../helpers/cryptoHelper');
 const importPost = require('./postImport');
+const { IMPORT_STATUS } = require('../../../constants/appData');
+
+const getStatus = async (user) => {
+  const { result } = await PostStatusModel.findOne({
+    filter: { user, status: { $in: [IMPORT_STATUS.WAITING_RECOVER, IMPORT_STATUS.ACTIVE] } },
+    projection: { _id: 1 },
+  });
+  if (result) return IMPORT_STATUS.PENDING;
+  return IMPORT_STATUS.ACTIVE;
+};
 
 const createPostTask = async ({
   posts, user, dailyLimit, host,
 }) => {
   const importId = createUUID();
+  const status = await getStatus(user);
 
   const { result } = await PostStatusModel.create({
     importId,
@@ -15,9 +26,10 @@ const createPostTask = async ({
     postsTotal: posts.length,
     dailyLimit,
     host,
+    status,
   });
-  await PostImportModel.insertMany(posts);
-  importPost({ importId, user });
+  await PostImportModel.insertMany(posts.map((p) => ({ ...p, importId })));
+  if (status === IMPORT_STATUS.ACTIVE) importPost({ importId, user });
 
   return { result };
 };
