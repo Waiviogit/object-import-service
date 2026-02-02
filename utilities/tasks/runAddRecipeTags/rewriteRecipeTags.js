e360c376c3ab94897945432d66c4b4560f710523
fesmofet <borees2@gmail.com>
2026-02-02 17:28:54 +0200

    update add task

--- utilities/tasks/runAddRecipeTags/rewriteRecipeTags.js
+++ utilities/tasks/runAddRecipeTags/rewriteRecipeTags.js
@@ -2,10 +2,13 @@ const { WObject } = require('../../../database').models;
 const _ = require('lodash');
 const { setTimeout } = require('timers/promises');
 const { vote } = require('../../hiveApi/broadcastUtil');
-const getCategoryItemsFields = require('../../services/parseObjectFields/fields/tagCategory');
-const { OBJECT_TYPES } = require('../../../constants/objectTypes');
+const { OBJECT_TYPES, OBJECT_FIELDS } = require('../../../constants/objectTypes');
 const { addField } = require('../../services/importObjectsService');
 const { votePowerValidation } = require('../../../validators/accountValidator');
+const { formField } = require('../../helpers/formFieldHelper');
+const { createUUID } = require('../../helpers/cryptoHelper');
+const { promptWithJsonSchema } = require('../../services/gptService');
+const { recipeTagsSchemaObject } = require('../../../constants/jsonShemaForAi');
 
 const VOTING_ACCOUNT = 'localguide';
 const MAX_POWER_RETRIES = 3;
@@ -28,6 +31,80 @@ const RECIPE_FILTER_ADD = {
   createdAt: { $lte: new Date('2025-11-05') },
   'authority.administrative': { $nin: ['mealprephive', 'dailydining'] },
   processed: false,
+};
+
+const tagsForRecipe = async (object, allFields) => {
+  const description = _.find(
+    allFields,
+    (f) => f.name === OBJECT_FIELDS.DESCRIPTION,
+  )?.body;
+  if (!description) return;
+
+  const ingredients = _.find(
+    allFields,
+    (f) => f.name === OBJECT_FIELDS.RECIPE_INGREDIENTS,
+  )?.body;
+
+  const fields = [];
+  let cuisineCategory = _.find(
+    allFields,
+    (f) => f.name === OBJECT_FIELDS.TAG_CATEGORY && f.body === 'Cuisine',
+  );
+  let prosCategory = _.find(
+    allFields,
+    (f) => f.name === OBJECT_FIELDS.TAG_CATEGORY && f.body === 'Pros',
+  );
+  if (!cuisineCategory) {
+    cuisineCategory = formField({
+      fieldName: OBJECT_FIELDS.TAG_CATEGORY,
+      locale: object.locale,
+      user: object.user,
+      body: 'Cuisine',
+      id: createUUID(),
+    });
+    fields.push(cuisineCategory);
+  }
+  if (!prosCategory) {
+    prosCategory = formField({
+      fieldName: OBJECT_FIELDS.TAG_CATEGORY,
+      locale: object.locale,
+      user: object.user,
+      body: 'Pros',
+      id: createUUID(),
+    });
+    fields.push(prosCategory);
+  }
+
+  const { result = {}, error } = await promptWithJsonSchema({
+    prompt: `create tags for following recipe ${object.name}, description: ${description}; ${ingredients ? `ingredients: ${ingredients}` : ''}`,
+    jsonSchema: recipeTagsSchemaObject,
+  });
+  if (error) throw new Error(error.message);
+  const { tags = [], cuisineTags = [] } = result;
+
+  for (const tag of tags) {
+    fields.push(formField({
+      fieldName: OBJECT_FIELDS.CATEGORY_ITEM,
+      locale: object.locale,
+      user: object.user,
+      body: tag.replace('#', ''),
+      tagCategory: 'Pros',
+      id: prosCategory.id,
+    }));
+  }
+
+  for (const tag of cuisineTags) {
+    fields.push(formField({
+      fieldName: OBJECT_FIELDS.CATEGORY_ITEM,
+      locale: object.locale,
+      user: object.user,
+      body: tag.replace('#', ''),
+      tagCategory: 'Cuisine',
+      id: cuisineCategory.id,
+    }));
+  }
+
+  return fields;
 };
 
 const rejectRecipeTags = async () => {
@@ -126,7 +203,14 @@ const addRecipeTags = async () => {
       if (!objects.length) break;
 
       for (const object of objects) {
-        let fields = await getCategoryItemsFields(
+        const description = _.find(object.fields, (f) => f.name === OBJECT_FIELDS.DESCRIPTION)?.body;
+        if (!description) {
+          console.log(`no description on object ${object.author_permlink}`);
+          await WObject.updateOne({ author_permlink: object.author_permlink }, { processed: true });
+          totalUpdated += 1;
+          continue;
+        }
+        let fields = await tagsForRecipe(
           {
             object_type: OBJECT_TYPES.RECIPE,
             locale: 'en-US',
@@ -135,11 +219,11 @@ const addRecipeTags = async () => {
           },
           object.fields,
         );
-        if (!fields.length) {
+        if (!fields?.length) {
           for (let attempt = 0; attempt < FIELDS_RETRY_COUNT; attempt += 1) {
             console.log(`No fields found for object ${object.author_permlink}, retry ${attempt + 1}/${FIELDS_RETRY_COUNT} after delay`);
             await setTimeout(FIELDS_RETRY_DELAY_MS);
-            fields = await getCategoryItemsFields(
+            fields = await tagsForRecipe(
               {
                 object_type: OBJECT_TYPES.RECIPE,
                 locale: 'en-US',
@@ -148,9 +232,9 @@ const addRecipeTags = async () => {
               },
               object.fields,
             );
-            if (fields.length) break;
+            if (fields?.length) break;
           }
-          if (!fields.length) {
+          if (!fields?.length) {
             throw new Error(`EXIT object ${object.author_permlink} due to empty fields after retries`);
           }
         }
